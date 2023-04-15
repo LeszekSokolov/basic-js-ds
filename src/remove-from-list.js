e8c4fb89013de5dcaa1282f627e37b8f3e2a757c
LeszekSokolov <Leszek.Sokolov@gmail.com>
2023-04-15 22:44:46 +0300

    feat: add remove from the list and stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,26 @@ const { NotImplementedError } = require("../extensions/index.js");
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError("Not implemented");
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let result;
+  let newList = [];
+
+  while (l) {
+    newList.push(l.value);
+    l = l.next;
+  }
+
+  newList
+    .filter((e) => e !== k)
+    .reverse()
+    .map((l, i) => {
+      result = {
+        value: l,
+        next: i !== 0 ? result : null,
+      };
+    });
+
+  return result;
 }
 
 module.exports = {
